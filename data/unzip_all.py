02dcbc4a170f1353dd22f15af76155491496e412
Kevin Tang <kvntng@gmail.com>
2020-03-11 20:30:15 -0400

    Added all data and script to unzip all at once

---
+++ data/unzip_all.py
@@ -0,0 +1,22 @@
+import os
+import glob
+import sh
+
+# CUR_DIR = os.cwd()
+unzip = sh.Command('unzip')
+mv = sh.Command('mv')
+rm = sh.Command('rm')
+
+for zipfile in glob.glob('*.zip'):
+	dir_name = zipfile.split('.')[0]
+	os.mkdir(dir_name)
+	mv(zipfile, dir_name)
+
+	os.chdir(dir_name)
+	unzip(zipfile)
+	# rm(zipfile)
+	os.chdir('..')
+
+# rm('*.zip')
+
+
